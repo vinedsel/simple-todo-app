559fe02456c0e9ca968ef1a401958df5fa58e416
Raymond Gregory <vinedsel@gmail.com>
2020-10-25 20:44:26 -0500

    Added code to file

---
+++ src/components/ToDoContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+class ToDoContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from Create React App</h1>
+                <p>I'm in a React Component!</p>
+            </div>
+        )
+    }
+}
+
+export default ToDoContainer;
