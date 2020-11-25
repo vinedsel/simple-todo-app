f6d5e638ec55555e3b6d1e5ef2b6f53b1d71119e
Raymond Gregory <vinedsel@gmail.com>
2020-11-24 18:24:44 -0600

    Created Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Simple ToDo App</h1>
+            <p>Please add to-do item(s) through the input field</p>
+        </header>
+    );
+}
+
+export default Header;
