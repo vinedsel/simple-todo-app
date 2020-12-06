f078260c1b75e2db977863c0f360ad344ee825de
Raymond Gregory <vinedsel@gmail.com>
2020-12-06 16:07:10 -0600

    Added an onChange event to checkboxes

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 class ToDoItem extends React.Component {
     render() {
         return (
-            <li><input type="checkbox" checked={this.props.todo.completed} />{this.props.todo.title}</li>
+            <li><input 
+                    type="checkbox" 
+                    checked={this.props.todo.completed}
+                    onChange={() => console.log("clicked")} 
+                />{this.props.todo.title}</li>
         );
     }
 }
