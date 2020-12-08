9aa418a437ea18fca5ee249e30dd0e7d0a48ff85
Raymond Gregory <vinedsel@gmail.com>
2020-12-07 23:11:36 -0600

    Added setState function with state updater for better reliability

--- src/components/ToDoContainer.jsx
+++ src/components/ToDoContainer.jsx
@@ -26,22 +26,22 @@ class ToDoContainer extends React.Component {
 
 
   handleChange = id => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map((todo) => {
         if (todo.id === id) {
           todo.completed = !todo.completed;
         }
         return todo;
-      })
-    });
-  }
+      }),
+    }));
+  };
 
   render() {
     return (
       <div>
         <Header />
         <ToDosList 
-          todos={this.state.todos} handleChangeProps={this,this.handleChange}
+          todos={this.state.todos} handleChangeProps={this.handleChange}
         />
       </div>
     );
