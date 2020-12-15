d7042c81e209a9547165b506553af87fc12d12b9
Raymond Gregory <vinedsel@gmail.com>
2020-12-14 21:40:04 -0600

    Small touches

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -8,19 +8,20 @@ class ToDoItem extends React.Component {
             opacity: 0.4,
             textDecoration: "line-through",
         };
+        const { completed, id, title } = this.props.todo;
         return (
             <li className="todo-item"><input 
                     type="checkbox" 
-                    checked={this.props.todo.completed}
-                    onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+                    checked={completed}
+                    onChange={() => this.props.handleChangeProps(id)}
                 />   
-                <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
-                <span style={this.props.todo.completed ? completedStyle : null}>
-                {this.props.todo.title} 
+                <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+                <span style={completed ? completedStyle : null}>
+                {title} 
                 </span>
             </li>
         );
-    }
-}
+    };
+};
 
 export default ToDoItem;
