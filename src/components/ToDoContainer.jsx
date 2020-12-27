f800221779ab3ff702350311b79e6ddc504d03e3
Raymond Gregory <vinedsel@gmail.com>
2020-12-26 18:20:29 -0600

    refactored addTodoItem to use axios post method

--- src/components/ToDoContainer.jsx
+++ src/components/ToDoContainer.jsx
@@ -36,14 +36,16 @@ class ToDoContainer extends React.Component {
   };
 
   addTodoItem = title => {
-    const newTodo = {
-      id: uuidv4(),
-      title: title,
-      completed: false
-    };
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    });
+    axios
+      .post('https://jsonplaceholder.typicode.com/todos', {
+        title: title,
+        completed: false,
+      })
+      .then(response =>
+        this.setState({
+          todos: [...this.state.todos, response.data],
+        })
+      );
   };
 
   componentDidMount() {
